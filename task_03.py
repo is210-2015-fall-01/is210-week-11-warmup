3058ded7bcd5a326f45535f03d3edad56c78946d
Shavon Brown <slb6968@hotmail.com>
2015-11-06 02:14:41 +0000

    Completed 4 Class warm-up tasks

---
+++ task_03.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A snapshot in time."""
+
+
+import produce
+
+
+class Apple(produce.Produce):
+    """A not-so-generic produce class is subclassed from produce.Produce."""
+
+    duration = 5356800
